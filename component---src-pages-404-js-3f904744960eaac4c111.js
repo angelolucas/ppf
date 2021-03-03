885f9601e08b2b1282832ece2f9a35264c263038
Ângelo Lucas <angelo.lrc@gmail.com>
2021-03-02 23:10:02 -0300

    new adjustments

--- component---src-pages-404-js-3f904744960eaac4c111.js
+++
@@ -1,2 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[2],{w2l6:function(n,e,t){"use strict";t.r(e);var c=t("q1tI"),a=t.n(c);e.default=function(){return Object(c.useEffect)((function(){return window.location.replace("/")}),[]),a.a.createElement(a.a.Fragment,null)}}}]);
-//# sourceMappingURL=component---src-pages-404-js-3f904744960eaac4c111.js.map
